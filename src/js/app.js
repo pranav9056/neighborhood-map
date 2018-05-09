6ebdc5afd55a9e3e2639b641db820b5a5ae6b930
Pranav <pranavjain3194@gmail.com>
2018-05-09 12:30:26 -0400

    fix: seperated map and view model

--- src/js/app.js
+++ src/js/app.js
@@ -1,59 +1,24 @@
 var app = app || {};
 
-function getLatLngFromAdd(add){
-  var url = "https://maps.googleapis.com/maps/api/geocode/json?address=%data%&key=AIzaSyBWj-fxuvnZLhzSIy2Zov-ztfJjMH9kvV8";
-  url = url.replace("%data%",add);
-  $.getJSON( url,function(data) {
-    console.log(ds);
-  } );
-}
+
+
+//app.homeAdd = "1215 Broadway Astoria";
+//var position = {lat:40.7668153 ,lng:-73.9341451};
 
 (function(){
-
-  app.mapDiv = document.getElementById('map');
-  app.homeAdd = "1215 Broadway Astoria";
-  var position = {lat:40.7668153 ,lng:-73.9341451};
-
-  app.initMap = function(){
-
-    app.map = new google.maps.Map(this.mapDiv, {
-      center: position,
-      zoom: 14,
-    });
-
-    app.houseMarker = new google.maps.Marker({
-      position: position,
-      title: app.homeAdd,
-      map: app.map,
-      icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-      animation: google.maps.Animation.DROP,
-      id: 1
-    });
-
-    app.service = new google.maps.places.PlacesService(app.map);
-    app.service.nearbySearch({
-      location: position,
-      radius: '1000',
-      type: ['restaurant']
-    }, app.viewModelObject.loadPlaces)
-  };
-
   app.viewModel = function(){
     var self = this;
+    self.homeAdd = ko.observable("1215 Broadway Astoria");
     self.places = ko.observableArray();
     self.loadPlaces = function(results,status){
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-          results.forEach(function(place){
-            self.places.push(place);
-          });
-        }
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        results.forEach(function(place){
+          self.places.push(place);
+        });
+      }
     }
   }
 
   app.viewModelObject = new app.viewModel()
   ko.applyBindings(app.viewModelObject);
-
-
-
-
 })();
--- src/js/map.js
+++ src/js/map.js
@@ -1,1 +1,47 @@
 var app = app || {}
+function getLatLngFromAdd(add){
+  var url = "https://maps.googleapis.com/maps/api/geocode/json?address=%data%&key=AIzaSyBWj-fxuvnZLhzSIy2Zov-ztfJjMH9kvV8";
+  url = url.replace("%data%",add);
+  $.getJSON( url,function(data) {
+    console.log(ds);
+  } );
+}
+(function(){
+
+  app.getPlaces = function(category){
+    var options = {
+      location: app.position,
+      radius: '1000',
+      type: category
+    }
+    app.service.nearbySearch(options, app.viewModelObject.loadPlaces);
+
+  }
+
+  app.initMap = function(){
+
+    app.mapDiv = document.getElementById('map');
+    app.position = {lat:40.7668153 ,lng:-73.9341451};
+    app.homeAdd = "1215 Broadway Astoria";
+
+    app.map = new google.maps.Map(app.mapDiv, {
+      center: app.position,
+      zoom: 14,
+    });
+
+    app.service = new google.maps.places.PlacesService(app.map);
+
+    app.houseMarker = new google.maps.Marker({
+      position: app.position,
+      title: app.homeAdd,
+      map: app.map,
+      icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+      animation: google.maps.Animation.DROP,
+      id: 1
+    });
+
+    app.getPlaces(['restaurant']);
+
+
+  };
+})();
