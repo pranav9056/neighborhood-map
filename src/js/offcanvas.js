714a9b3067c18e61c0b47328e46c5cbdf0bd9cc3
Pranav <pranavjain3194@gmail.com>
2018-05-13 15:13:03 -0400

    made navbar and sidebar responsive

--- src/js/offcanvas.js
+++ src/js/offcanvas.js
@@ -2,6 +2,7 @@ $(function () {
   'use strict'
 
   $('[data-toggle="offcanvas"]').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open')
+    $('.offcanvas-collapse').toggleClass('d-none');
+    $('#map').toggleClass('col-12 col-9 d-none d-sm-block');
   })
 })
