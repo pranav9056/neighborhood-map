39c9a1b0f43ef299c728f2842cede39b427984c9
Pranav <pranavjain3194@gmail.com>
2018-05-13 22:42:52 -0400

    ran grunt

---
+++ dist/js/offcanvas.js
@@ -0,0 +1,1 @@
+$(function(){"use strict";$('[data-toggle="offcanvas"]').on("click",function(){$(".offcanvas-collapse").toggleClass("d-none"),$("#map").toggleClass("col-12 col-9 d-none d-sm-block")})});
